0fd9d6060735587667bf6ee658f1ba239e23b671
Nina Palma <ninapalmadev@gmail.com>
2022-10-03 14:23:30 -0300

    cria função para converter outras moedas para Real

--- src/br/com/alura/conversorMoedas/FuncaoConverter.java
+++ src/br/com/alura/conversorMoedas/FuncaoConverter.java
@@ -12,26 +12,26 @@ public class FuncaoConverter {
 	}
 
 	public void ConverterReaisParaEuro(double valorRecebido) {
-		double moedaEuro = valorRecebido / 5.14;
+		double moedaEuro = valorRecebido / 5.16;
 		moedaEuro = (double) Math.round(moedaEuro * 100d) / 100;
 		JOptionPane.showMessageDialog(null, "Você tem $: " + moedaEuro + " Euros");
 		;
 	}
 
 	public void ConverterReaisParaLibraEsterlina(double valorRecebido) {
-		double moedaLibraEsterlina = valorRecebido / 5.96;
+		double moedaLibraEsterlina = valorRecebido / 5.83;
 		moedaLibraEsterlina = (double) Math.round(moedaLibraEsterlina * 100d) / 100;
 		JOptionPane.showMessageDialog(null, "Você tem $ " + moedaLibraEsterlina + " Libra Esterlina");
 	}
 
 	public void ConverterReaisParaPesoArgentino(double valorRecebido) {
-		double moedaPesoArgentino = valorRecebido / 0.036;
+		double moedaPesoArgentino = valorRecebido / 0.035;
 		moedaPesoArgentino = (double) Math.round(moedaPesoArgentino * 100d) / 100;
 		JOptionPane.showMessageDialog(null, "Você tem $ " + moedaPesoArgentino + " Pesos Argentinos");
 	}
 
 	public void ConverterReaisParaPesoChileno(double valorRecebido) {
-		double moedaPesoChileno = valorRecebido / 0.0057;
+		double moedaPesoChileno = valorRecebido /0.0055;
 		moedaPesoChileno = (double) Math.round(moedaPesoChileno * 100d) / 100;
 		JOptionPane.showMessageDialog(null, "Você tem $ " + moedaPesoChileno + " Pesos Chilenos");
 	}
